634e7b0e42b11d869c5127cab1073f940d2e37a4
prklm10 <pradumraj360@gmail.com>
2023-08-07 11:17:04 +0530

    fixing filepath

--- percy-xcui/Sources/PercyXcui/metadata/Metadata.swift
+++ percy-xcui/Sources/PercyXcui/metadata/Metadata.swift
@@ -66,7 +66,7 @@ internal class Metadata {
   }
 
   func readDataFromJSONFile(forKey key: String) -> Int {
-    guard let filePath = Bundle.main.path(forResource: "config", withExtension: "json") else {
+    guard let filePath = Bundle.main.path(forResource: "devices", ofType: "json") else {
       print("JSON file not found.")
       return 100
     }
